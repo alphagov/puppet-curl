d93cdf4d65c84101bf6fdf4b6851106f792ef88a
Simon Page <simon.page@lovehoney.co.uk>
2013-08-21 09:51:35 +0100

    Added https_proxy environment variable support, now requires stdlib

--- manifests/authfetch.pp
+++ manifests/authfetch.pp
@@ -2,9 +2,9 @@
 # Definition: curl::authfetch
 #
 # This class will download files from the internet.  You may define a web proxy
-# using $http_proxy if necessary. Username must be provided. And the user's
-# password must be stored in the password variable within the .curlrc file.
-#
+# using $http_proxy or $https_proxy if necessary. Username must be provided. 
+# And the user's password must be stored in the password variable within the
+# .curlrc file.
 ################################################################################
 define curl::authfetch(
     $source,
@@ -16,13 +16,22 @@ define curl::authfetch(
     $verbose=false) {
         
   include curl
+  include stdlib
 
-  if $http_proxy {
-    $environment = [ "HTTP_PROXY=$http_proxy", "http_proxy=$http_proxy" ]
+  if $::http_proxy {
+    $env1 = [ "HTTP_PROXY=$::http_proxy", "http_proxy=$::http_proxy" ]
   }
   else {
-    $environment = []
+    $env1 = []
   }
+
+  if $::https_proxy {
+    $env2 = [ "HTTPS_PROXY=$::https_proxy", "https_proxy=$::https_proxy" ]
+  }
+  else {
+    $env2 = []
+  }
+
 
   $verbose_option = $verbose ? {
     true  => "--verbose",
@@ -38,7 +47,7 @@ define curl::authfetch(
     command     => "curl $verbose_option $options --config /tmp/curlrc-$name --output $destination $source",
     timeout     => $timeout,
     unless      => "test -s $destination",
-    environment => $environment,
+    environment => concat($env1, $env2),
     path        => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin",
     require     => Class[curl],
   }
--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -2,18 +2,27 @@
 # Definition: curl::fetch
 #
 # This class will download files from the internet.  You may define a web proxy
-# using $http_proxy if necessary.
+# using $http_proxy or $https_proxy if necessary.
 #
 ################################################################################
 define curl::fetch($source,$destination,$options="",$timeout="0",$verbose=false) {
   include curl
+  include stdlib
 
   if $::http_proxy {
-    $environment = [ "HTTP_PROXY=$::http_proxy", "http_proxy=$::http_proxy" ]
+    $env1 = [ "HTTP_PROXY=$::http_proxy", "http_proxy=$::http_proxy" ]
   }
   else {
-    $environment = []
+    $env1 = []
   }
+
+  if $::https_proxy {
+    $env2 = [ "HTTPS_PROXY=$::https_proxy", "https_proxy=$::https_proxy" ]
+  }
+  else {
+    $env2 = []
+  }
+
 
   $verbose_option = $verbose ? {
     true  => "--verbose",
@@ -24,7 +33,7 @@ define curl::fetch($source,$destination,$options="",$timeout="0",$verbose=false)
     command     => "curl $verbose_option $options --output $destination $source",
     timeout     => $timeout,
     unless      => "test -s $destination",
-    environment => $environment,
+    environment => concat($env1, $env2),
     path        => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin",
     require     => Class[curl],
   }
