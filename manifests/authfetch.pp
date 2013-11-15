039c917f2fde8315c0acbf32794b22595a5bed4a
Simon Page <simon.page@lovehoney.co.uk>
2013-11-15 14:40:32 +0000

    Tidying up for puppet-lint

--- manifests/authfetch.pp
+++ manifests/authfetch.pp
@@ -2,31 +2,31 @@
 # Definition: curl::authfetch
 #
 # This class will download files from the internet.  You may define a web proxy
-# using $http_proxy or $https_proxy if necessary. Username must be provided. 
+# using $http_proxy or $https_proxy if necessary. Username must be provided.
 # And the user's password must be stored in the password variable within the
 # .curlrc file.
 ################################################################################
 define curl::authfetch(
-    $source,
-    $destination,
-    $user,
-    $options="",
-    $password="",
-    $timeout="0",
-    $verbose=false) {
-        
+    $source       = undef,
+    $destination  = undef,
+    $user         = undef,
+    $options      = '',
+    $password     = '',
+    $timeout      = '0',
+    $verbose      = false) {
+
   include curl
   include stdlib
 
   if $::http_proxy {
-    $env1 = [ "HTTP_PROXY=$::http_proxy", "http_proxy=$::http_proxy" ]
+    $env1 = [ "HTTP_PROXY=${::http_proxy}", "http_proxy=${::http_proxy}" ]
   }
   else {
     $env1 = []
   }
 
   if $::https_proxy {
-    $env2 = [ "HTTPS_PROXY=$::https_proxy", "https_proxy=$::https_proxy" ]
+    $env2 = [ "HTTPS_PROXY=${::https_proxy}", "https_proxy=${::https_proxy}" ]
   }
   else {
     $env2 = []
@@ -34,22 +34,21 @@ define curl::authfetch(
 
 
   $verbose_option = $verbose ? {
-    true  => "--verbose",
-    false => "--silent --show-error"
+    true  => '--verbose',
+    false => '--silent --show-error'
   }
 
-  file { "/tmp/curlrc-$name":
+  file { "/tmp/curlrc-${name}":
     owner   => root,
-    mode    => "0600",
-    content => "user = $user:$password",
+    mode    => '0600',
+    content => "user = ${user}:${password}",
   } ->
-  exec { "curl-$name":
-    command     => "curl $verbose_option $options --config /tmp/curlrc-$name --output $destination $source",
+  exec { "curl-${name}":
+    command     => "curl ${verbose_option} ${options} --config /tmp/curlrc-${name} --output ${destination} ${source}",
     timeout     => $timeout,
-    unless      => "test -s $destination",
+    creates     => $destination,
     environment => concat($env1, $env2),
-    path        => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin",
+    path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin',
     require     => Class[curl],
   }
 }
-
--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -5,36 +5,42 @@
 # using $http_proxy or $https_proxy if necessary.
 #
 ################################################################################
-define curl::fetch($source,$destination,$options="",$timeout="0",$verbose=false) {
+define curl::fetch(
+  $source      = undef,
+  $destination = undef,
+  $options     = '',
+  $timeout     = '0',
+  $verbose     = false
+  ) {
+
   include curl
   include stdlib
 
   if $::http_proxy {
-    $env1 = [ "HTTP_PROXY=$::http_proxy", "http_proxy=$::http_proxy" ]
+    $env1 = [ "HTTP_PROXY=${::http_proxy}", "http_proxy=${::http_proxy}" ]
   }
   else {
     $env1 = []
   }
 
   if $::https_proxy {
-    $env2 = [ "HTTPS_PROXY=$::https_proxy", "https_proxy=$::https_proxy" ]
+    $env2 = [ "HTTPS_PROXY=${::https_proxy}", "https_proxy=${::https_proxy}" ]
   }
   else {
     $env2 = []
   }
 
-
   $verbose_option = $verbose ? {
-    true  => "--verbose",
-    false => "--silent --show-error"
+    true  => '--verbose',
+    false => '--silent --show-error'
   }
 
-  exec { "curl-$name":
-    command     => "curl $verbose_option $options --output $destination $source",
+  exec { "curl-${name}":
+    command     => "curl ${verbose_option} ${options} --output ${destination} ${source}",
     timeout     => $timeout,
-    unless      => "test -s $destination",
+    creates     => $destination,
     environment => concat($env1, $env2),
-    path        => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin",
+    path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin:/opt/local/bin',
     require     => Class[curl],
   }
 }
