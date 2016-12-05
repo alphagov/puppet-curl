38eed380819ebf6a50668ec600953dc9e15d79c2
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-12-05 16:22:27 +0000

    Switch default to present
    Present and installed are the same thing, unless they're being used
    in the context of a package resource in which case they end up
    being duplicate resource definitions.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 # This class will install curl - a tool used to download content from the web.
 #
 ################################################################################
-class curl($version='installed') {
+class curl($version='present') {
 
   ensure_packages(['curl'], {
     'ensure' => $version,
